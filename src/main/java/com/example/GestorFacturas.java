54431686219d6dce76ad8a958582906cb60e17b5
duvan1026 <duvancastro1026@gmail.com>
2023-03-28 09:27:50 -0500

    scanner file pack for add beans in xml

--- src/main/java/com/example/GestorFacturas.java
+++ src/main/java/com/example/GestorFacturas.java
@@ -1,18 +1,22 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class GestorFacturas {
 
     // 1. atributos
     Calculadora calculadora;
-    String nombre;
 
 
     // 2. constructores
-    public GestorFacturas(Calculadora calculadora, String nombre){
+    @Autowired // Para establecer cual constructor se ejecutara
+    public GestorFacturas(Calculadora calculadora){
 
         System.out.println("Ejecutando constructor GestorFacturas");
         this.calculadora = calculadora;
-        this.nombre = nombre;
 
     }
 
